0f7668d27c7dc3da802f46a2cf13b428baa7c362
leenaahmadi <leenaahmadi@leenas-mbp.lan>
2022-01-04 14:45:07 -0800

    Day 10 Question 4 HW

---
+++ src/test/java/day10hw/Question4.java
@@ -0,0 +1,21 @@
+package day10hw;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+
+		getDoWhileLoopResult(1, 10);
+
+	}
+
+	public static void getDoWhileLoopResult(int minNum, int maxNum) {
+
+		do {
+			System.out.println(minNum);
+			minNum++;
+
+		} while (minNum <= maxNum);
+
+	}
+
+}
